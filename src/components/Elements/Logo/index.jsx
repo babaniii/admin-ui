f9e11e4f1db07f9cd155cfbd5310f5d4db6055d1
Rafi Tech <rendyroxxy464@gmail.com>
2024-10-28 13:33:16 +0700

    Completed

--- src/components/Elements/Logo/index.jsx
+++ src/components/Elements/Logo/index.jsx
@@ -3,7 +3,7 @@ const Logo = (props) => {
 
   return (
     <div
-      className={`flex justify-center font-poppins tracking-wide mt-6 ${variant}`}
+      className={`flex justify-center font-poppins tracking-wide mt-4 ${variant}`}
     >
       <span className="font-bold ">FINE</span>bank
       <span className="font-bold">.IO</span>
