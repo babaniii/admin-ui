03388e36fd9dad6621d86379f78149b0b8b33759
babaniii <rafirizwanrabbani@gmail.com>
2025-01-09 01:22:07 +0700

    uas

--- src/pages/expanses.jsx
+++ src/pages/expanses.jsx
@@ -1,4 +1,4 @@
-import Card from "../components/Elements/Card_2";
+import Card2 from "../components/Elements/Card_2";
 import Card3 from "../components/Elements/Card_3";
 import MainLayout from "../components/Layouts/MainLayout";
 
@@ -7,17 +7,18 @@ const ExpencessPage = () => {
     <MainLayout type="dashboard">
       {/* top content start*/}
         <div className="mb-4 sm:w-full">
-          <Card />
+          <Card2 />
         </div>
       {/* top content end*/}
       {/* bottom content start*/}
+      <div className="text-lg text-gray-02 mb-2">Expenses Breakdown</div>
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mb-4">
-      <Card />
-      <Card content="deskripsi" />
-      <Card />
-      <Card />
-      <Card />
-      <Card content="deskripsi" />
+      <Card3 />
+      <Card3 content="deskripsi" />
+      <Card3 />
+      <Card3 />
+      <Card3 />
+      <Card3 content="deskripsi" />
       </div>
       {/* bottom content end*/}
     </MainLayout>
